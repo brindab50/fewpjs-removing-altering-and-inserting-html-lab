75614999de2b116b520b25dc2493ba9e20854fb4
Brinda Behal <brindaxo5@gmail.com>
2021-10-10 16:17:45 -0400

    commit tree 3e50ca202e73f52285cc92c2b05c639491eb3ae6

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+let main = document.getElementById("main")
+main.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "brinda is the champion"
+
+
+// let newHeader = document.createElement("h1")
+// let inner = document.createTextNode("victory")
+// newHeader.appendChild(inner)
